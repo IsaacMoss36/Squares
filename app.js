624381eebc358315b77426ee7eec2d73b9b0ebd9
IsaacMoss36 <isaacmoss08@gmail.com>
2021-06-16 19:08:30 -0600

    adding all files

---
+++ app.js
@@ -0,0 +1,63 @@
+$('#cell1').on('click', function(){
+    $(`#cell1`).toggleClass('darken');
+})
+
+$('#cell2').on('click', function(){
+    $(`#cell2`).toggleClass('darken');
+})
+
+$('#cell3').on('click', function(){
+    $(`#cell3`).toggleClass('darken');
+})
+
+$('#cell4').on('click', function(){
+    $(`#cell4`).toggleClass('darken');
+})
+
+$('#cell5').on('click', function(){
+    $(`#cell5`).toggleClass('darken');
+})
+
+$('#cell6').on('click', function(){
+    $(`#cell6`).toggleClass('darken');
+})
+
+$('#cell7').on('click', function(){
+    $(`#cell7`).toggleClass('darken');
+})
+
+$('#cell8').on('click', function(){
+    $(`#cell8`).toggleClass('darken');
+})
+
+$('#cell9').on('click', function(){
+    $(`#cell9`).toggleClass('darken');
+})
+
+$('#cell10').on('click', function(){
+    $(`#cell10`).toggleClass('darken');
+})
+
+$('#cell11').on('click', function(){
+    $(`#cell11`).toggleClass('darken');
+})
+
+$('#cell12').on('click', function(){
+    $(`#cell12`).toggleClass('darken');
+})
+
+$('#cell13').on('click', function(){
+    $(`#cell13`).toggleClass('darken');
+})
+
+$('#cell14').on('click', function(){
+    $(`#cell14`).toggleClass('darken');
+})
+
+$('#cell15').on('click', function(){
+    $(`#cell15`).toggleClass('darken');
+})
+
+$('#cell16').on('click', function(){
+    $(`#cell16`).toggleClass('darken');
+})
